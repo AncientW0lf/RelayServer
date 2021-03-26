56bdd7635c974045a0aba6870975b06669976841
LunarDiver <25Prozent@gmail.com>
2021-03-26 16:58:11 +0100

    Implemented relay for test ip.

--- Controllers/RelayController.cs
+++ Controllers/RelayController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Net.Http;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace RelayServer.Controllers
 {
@@ -11,17 +9,25 @@ namespace RelayServer.Controllers
     public class RelayController : ControllerBase
     {
         [HttpGet("{*url}")]
-        public IActionResult Get(string url)
+        public async Task<IActionResult> Get(string url)
         {
-            string requestUrl = string.Concat(
-                        Request.Scheme,
-                        "://",
-                        Request.Host,
-                        Request.PathBase,
-                        Request.Path,
-                        Request.QueryString);
+            // string requestUrl = string.Concat(
+            //             Request.Scheme,
+            //             "://",
+            //             Request.Host,
+            //             Request.PathBase,
+            //             Request.Path,
+            //             Request.QueryString);
 
-            return Ok($"Received: {url} ({requestUrl})");
+            using var client = new HttpClient();
+
+            //TODO: Replace fixed IP with configuration file for links with domain names and local IPs.
+            HttpResponseMessage res = await client.GetAsync($"http://192.168.178.33/{url}");
+            string type = res.Content.Headers.GetValues("Content-Type").First();
+            byte[] content = await res.Content.ReadAsByteArrayAsync();
+
+            return new FileContentResult(content, type);
+            //Ok($"Received: {url} ({requestUrl})");
         }
     }
 }
