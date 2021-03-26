5cb094ee0f1608be83f0e8776ffed287d7c445d3
LunarDiver <25Prozent@gmail.com>
2021-03-26 09:17:59 +0100

    Added basic controller.

---
+++ Controllers/RelayController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RelayServer.Controllers
+{
+    [ApiController]
+    public class RelayController : ControllerBase
+    {
+        [HttpGet("{*url}")]
+        public IActionResult Get(string url)
+        {
+            string requestUrl = string.Concat(
+                        Request.Scheme,
+                        "://",
+                        Request.Host,
+                        Request.PathBase,
+                        Request.Path,
+                        Request.QueryString);
+
+            return Ok($"Received: {url} ({requestUrl})");
+        }
+    }
+}
